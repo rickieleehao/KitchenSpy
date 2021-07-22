611710a724962936504f842ed17b8371841d270e
hyy170190 <70197056+hyy170190@users.noreply.github.com>
2021-07-22 11:41:55 +0800

    BottomTab
    upload huiYingBottomTab

---
+++ sample/huiYingBottomTab/ShoppingList.js
@@ -0,0 +1,13 @@
+import 'react-native-gesture-handler';
+import * as React from 'react';
+import { Text, View } from 'react-native';
+
+function ShoppingList() {
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text>Shopping List!</Text>
+        </View>
+    );
+}
+
+export default ShoppingList;
